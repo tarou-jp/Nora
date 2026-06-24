e12c2d4390ca83f685ce6bdba03d29de4fa57365
tarou-jp <itohisahideki@gmail.com>
2026-06-24 23:53:34 +0900

    Initial Commit

---
+++ Nora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nora
+//
+//  Created by 糸久秀喜 on 2026/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nora/NoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoraApp.swift
+//  Nora
+//
+//  Created by 糸久秀喜 on 2026/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
